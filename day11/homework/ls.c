a6cc55fdec339d41ef6e846c001936b585a9f9d0
SuYou <1806992332@qq.com>
2025-03-09 13:21:50 +0800

    update ls.c

--- day11/homework/ls.c
+++ day11/homework/ls.c
@@ -2,7 +2,14 @@
 
 int main(int argc,char *argv[])
 {
-    system("ls");    
+    //system("ls");    
+	// 目录流
+	DIR * dirp = opendir(".");
+	struct dirent * direntp;
+	while((direntp = readdir(dirp))!=NULL){
+		printf("%s\n",direntp->d_name);
+	}
+	closedir(dirp);
     return 0;
 }
 
