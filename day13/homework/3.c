d6cd4d352ef3b4f533c5c16017aef06d74b5e54c
SuYou <1806992332@qq.com>
2025-03-09 14:01:21 +0800

    update 3.c 3

--- day13/homework/3.c
+++ day13/homework/3.c
@@ -1,5 +1,4 @@
 #include <2025Linux.h>
-#include <signal.h>
 
 void handler(int signum){
     printf("signum = %d\n",signum);
@@ -8,21 +7,21 @@ void handler(int signum){
 int main(int argc,char *argv[])
 {
     signal(SIGINT,handler);
-    sigset_t set;
+    sigset_t set,oldset;
     sigemptyset(&set);
     sigaddset(&set,SIGINT);
-    sigprocmask(SIG_SETMASK,&set,NULL);
+    sigprocmask(SIG_BLOCK,&set,&oldset);
     sleep(5);
-    sigset_t set2;
-    sigpending(&set2);
-    if(sigismember(&set2,SIGINT)){
+    sigset_t pendingset;
+    sigpending(&pendingset);
+    if(sigismember(&pendingset,SIGINT)){
         printf("SIGINT is in pengding!\n");
-    }
-    sigdelset(&set,SIGINT);
-    sigprocmask(SIG_SETMASK,&set,NULL);
+	}else{
+        printf("SIGINT is not in pengding!\n");
+	}
+    sigprocmask(SIG_SETMASK,&oldset,NULL);
     while(1){
         sleep(1);
     }
     return 0;
 }
-
